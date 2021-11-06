0229e2bc901158aa10c3f0bb41e7de017be6965d
LiLy Perera <lilycapetillo86@gmail.com>
2021-11-06 00:04:04 -0400

    Nuevo Cambio

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Pokemones({pokemones}) {
     console.log(pokemones)
     return (
         <div>
-            <p> Pokemones </p>
+            <p> Mi App de Pokemones </p>
             <ul>
                 {pokemones.map(pokemon =>  <Pokemon pokemon={pokemon} key={pokemon.name}/>)}
             </ul>
